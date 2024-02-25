a8d8c9198db13484d91d49c71d58541dcda32e8f
Elizabeth <itsybitsy1121@outlook.com>
2024-02-25 13:14:57 -0800

    made changes to the JS/main.js file and index.html file

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -11,3 +11,12 @@ function My_first_function() {
     var str = "This is the button text!";
     document.getElementById("Button_text").innerHTML = str;
 }
+
+// ---------------------------------------------------
+
+var sent1 = "This is the beginning of the string"; // Define a variable with a string value
+var sent2 = "and this is the end of the string"; // Define another variable with a string value
+
+var concatenatedSentence = sent1 + " " + sent2; // Concatenate the two sentences with a space in between
+var sum = 5 + 10; // Calculate the sum of 5 and 10
+
