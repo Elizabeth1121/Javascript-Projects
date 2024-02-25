4804b2e390c4480002b08026f6f649838c7f34d3
Elizabeth <itsybitsy1121@outlook.com>
2024-02-25 13:08:26 -0800

    first commit

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+// Declare a variable with a string value
+var message = "This is a string";
+var messagestr = "\"hello world\"";
+var messageconcat = "This is a string" + " and this is a string";
+var family = "The Arezzinis", dad = "Jeremiah", mom = "Hermoine", daughter = "Penny", son = "Zorro";
+
+window.alert(dad); // Display the message in an alert box
+document.write(3 + 3); // Display the message in the document
+
+function My_first_function() {
+    var str = "This is the button text!";
+    document.getElementById("Button_text").innerHTML = str;
+}
